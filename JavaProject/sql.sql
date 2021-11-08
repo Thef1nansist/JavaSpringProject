7f99d5cefb8a84e31482ab5a6ae5127d3e8c6b11
Thef1nansist <vladsavhchenko994@gmail.com>
2021-11-08 14:35:56 +0300

    added new UI

--- JavaProject/sql.sql
+++
@@ -1,9 +0,0 @@
-select * from users;
-select * from user_roles;
-select * from roles;
-insert into roles(name) values('ROLE_USER');
-insert into roles(name) values('ROLE_ADMIN');
-
-select * from employee;
-
-
